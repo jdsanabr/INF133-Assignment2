4b89ebc9101cc3a3b1fab357694eca1263f16532
jdsanabr <jonathandavidsanabria@gmail.com>
2021-01-30 14:18:23 -0800

    Update about.js
    Added a commented structure for needed code additions to file.

--- js/about.js
+++ js/about.js
@@ -11,7 +11,52 @@ function parseTweets(runkeeper_tweets) {
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('numberTweets').innerText = tweet_array.length;
+
+
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	//Update 'firstDate' tag
+	document.getElementById('firstDate').innerText = 'September 23, 2018';
+	//
+
+	//Update 'lastDate' tag
+	document.getElementById('lastDate').innerText = 'September 30, 2018';
+	//
+
+
+	//Update 'completedEvents' tag
+	//
+
+	//Update 'completedEventsPct' tag
+	//
+
+
+	//Update 'liveEvents' tag
+	//
+
+	//Update 'liveEventsPct' tag
+	//
+
+
+	//Update 'achievenemts' tag
+	//
+
+	//Update 'achievementsPct' tag
+	//
+
+
+	//Update 'miscellaneous' tag
+	//
+
+	//Update 'miscellaneousPct' tag
+	//
+
+
+	//Update 'written' tag
+	//
+
+	//Update 'writtenPct tag
+	//
 }
 
 //Wait for the DOM to load
